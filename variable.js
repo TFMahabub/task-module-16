55caa37175e98747af767c42a92ab5c537b80da2
TFMahabub <rjmahabub543@gmail.com>
2022-07-31 15:54:34 +0600

    practice-2

--- variable.js
+++ variable.js
@@ -7,3 +7,26 @@ allCost = orange1kg + apple1kg
 equalMoney = mainMoney - allCost
 
 console.log(equalMoney)
+
+
+//prectice-2
+
+// ------all-subject------
+var mathematicsMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+
+var allSunjectMarks = mathematicsMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks
+
+average = allSunjectMarks / 5
+
+average = average.toFixed(2)
+
+average = parseFloat(average)
+
+console.log(average)
+
+
+//prectice-3
